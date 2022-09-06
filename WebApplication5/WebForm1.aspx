8a44c9107950930bad3db38e201784bac86da521
shuja haider <syrshuja@gmail.com>
2022-09-06 20:00:42 +0500

    Add project files.

---
+++ WebApplication5/WebForm1.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <div>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.0/sweetalert.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.0/sweetalert.min.css"
+       
+        rel="stylesheet" type="text/css" />
+</div>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            height: 55px;
+        }
+        .auto-style3 {
+            width: 200px;
+        }
+        .auto-style4 {
+            margin-right: 0px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+     
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" Height="40px" OnTextChanged="DropDownList1_TextChanged" Width="149px">
+        </asp:DropDownList>
+        <table class="auto-style1">
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:DropDownList ID="DropDownList2" runat="server" Height="50px" Width="149px">
+                    </asp:DropDownList>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:DropDownList ID="DropDownList3" runat="server" Height="50px" Width="149px">
+                    </asp:DropDownList>
+                </td>
+                <td class="auto-style2"></td>
+            </tr>
+        </table>
+     
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style3">Search by ID</td>
+                <td>
+                    <asp:TextBox ID="SearchTxt" runat="server"></asp:TextBox>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="View" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Name</td>
+                <td>
+                    <asp:TextBox ID="Nametxt" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Desgination</td>
+                <td>
+                    <asp:TextBox ID="DesgiantionTxt" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Email</td>
+                <td>
+                    <asp:TextBox ID="Emailtxt" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Age</td>
+                <td>
+                    <asp:TextBox ID="Agetxt" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+     
+        <asp:GridView ID="GridView1" runat="server" CssClass="auto-style4" Height="204px" Width="629px">
+        </asp:GridView>
+     
+    </form>
+</body>
+</html>
---
+++ WebApplication5/WebForm1.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        String cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindData();
+                BindGridEMployeeData();
+            }
+
+        }
+        public void BindData()
+        {
+            SqlConnection sql = new SqlConnection(cs);
+            String qry = "select name  from EmployeesDetails";
+            SqlDataAdapter da = new SqlDataAdapter(qry, sql);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DropDownList1.DataSource = dt;
+            DropDownList1.DataTextField = "Name";
+            //   DropDownList1.DataValueField = "id";
+            DropDownList1.DataBind();
+
+            DropDownList1.Items.Insert(0, new ListItem("--Select Employee--", "0"));
+
+
+
+        }
+
+        public void BindDesgination()
+        {
+            SqlConnection sql = new SqlConnection(cs);
+            String qry = "select * from EmployeesDetails";
+            SqlDataAdapter da = new SqlDataAdapter(qry, sql);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DropDownList2.DataSource = dt;
+            DropDownList2.DataTextField = "Desgination";
+
+            DropDownList2.DataBind();
+
+            DropDownList2.Items.Insert(0, new ListItem("--Select Desgination--", "0"));
+
+
+
+        }
+
+        public void BindAttributes()
+        {
+            int id = Convert.ToInt32(DropDownList1.SelectedIndex);
+
+            SqlConnection sql = new SqlConnection(cs);
+            String qry = "select * from employeeRelated where id = @id";
+            SqlCommand cmd = new SqlCommand(qry, sql);
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DropDownList3.DataSource = dt;
+            DropDownList3.DataValueField = "id";
+            DropDownList3.DataTextField = "Behaviour";
+
+            DropDownList3.DataBind();
+
+            DropDownList3.Items.Insert(0, new ListItem("--Check Your Attribute--", "0"));
+
+
+
+        }
+
+        protected void DropDownList1_TextChanged(object sender, EventArgs e)
+        {
+            BindDesgination();
+
+            BindAttributes();
+        }
+
+
+        public void BindSearch()
+        {
+            try
+            {
+                SqlConnection sql = new SqlConnection(cs);
+                String qry = "select name, desgination , age , Email from EmployeesDetails where id = @id";
+                SqlCommand cmd = new SqlCommand(qry, sql);
+                cmd.Parameters.AddWithValue("@id", SearchTxt.Text);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                Nametxt.Text = dt.Rows[0]["name"].ToString();
+                DesgiantionTxt.Text = dt.Rows[0]["Desgination"].ToString();
+                Agetxt.Text = dt.Rows[0]["age"].ToString();
+                Emailtxt.Text = dt.Rows[0]["Email"].ToString();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex);
+
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            BindSearch();
+            BindGridEMployeeData();
+        }
+        
+        public void BindGridEMployeeData()
+        {
+            SqlConnection sql = new SqlConnection(cs);
+            String qry = "select name, desgination , age , Email from EmployeesDetails where id = @id";
+            SqlCommand cmd = new SqlCommand(qry, sql);
+            cmd.Parameters.AddWithValue("@id", SearchTxt.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
